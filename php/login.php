5182c2a4d4051a47cd9af1d01263978a0cdfb214
Florian APAVOU <florian9303@gmail.com>
2019-05-27 17:08:18 +0200

    ajout de quelques images pour le fun et login.php

--- php/login.php
+++ php/login.php
@@ -1,36 +1,86 @@
-<div class="row">
-	<div id="connexion" class="col-lg-6">
-		  <form>
-		      <label for="pseudo"><b>Pseudo : </b></label>
-		      <input type="text" placeholder="Entrez votre pseudo" name="pseudo" required>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<title>Connexion</title>
 
-		      <label for="mdp"><b>Mot de passe : </b></label>
-		      <input type="password" placeholder="Entrez votre mot de passe" name="mdp" required>
+	<?php
+	include "../php/includes.php";
+	include "../php/fonctions.php";
+	?>
+</head>
+<body>
+	<div class="navbar navbar-expand-lg" id="navbar-content">
 
-		      <button type="submit">Connexion</button>
-		      <label>
-		        <input type="checkbox" checked="checked" name="remember"> Se rappeler de moi
-		      </label>
-		      <span><a href="#">Mot de passe oublié ?</a></span>
-		  </form>
-	</div>
-	<div id="inscription" class="col-lg-6">
-		<div>
-		  <form>
-		      <label for="pseudo"><b>Pseudo : </b></label>
-		      <input type="text" placeholder="Pseudo" name="pseudo" required>
-
-		      <label for="mdp"><b>Mot de passe : </b></label>
-		      <input type="password" placeholder="Mot de passe" name="mdp" required>
-
-		      <label for="mdp2"><b>Retapez votre mot de passe : </b></label>
-		      <input type="password" placeholder="Mot de passe" name="mdp2" required>
-
-		      <label for="email"><b>Adresse email : </b></label>
-		      <input type="password" placeholder="Email" name="email" required>
-
-		      <button type="submit">Inscription</button>
-		  </form>
+		<!-- Colonne Icone -->
+		<div class="col-lg-3 col-md-12 col-sm-12 col-xs-12">
+			<!-- Logo Navbar -->
+			<a class="navbar-brand" href="index.php">Médiathèque <br> Karine <br> Lemarchand</a>
 		</div>
 	</div>
-</div>
+
+
+	<div class="container">
+		<div class="row formsCo">
+			<div id="connexion" class="col-lg-5 verticalLine">
+				<fieldset>
+					<legend>Connexion</legend>
+					<form>
+						<div class="form-group">
+							<label for="inputEmail1">Email</label>
+							<input type="email" class="form-control" id="inputEmail1" aria-describedby="emailHelp" placeholder="Entrer email" required>
+							<small id="emailHelp" class="form-text text-muted">Nous ne partagerons pas votre adresse email.</small>
+						</div>
+						<div class="form-group">
+							<label for="exampleInputPassword1">Mot de Passe</label>
+							<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de Passe" required>
+						</div>
+						<div class="form-group form-check">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">Se rappeler de moi</label>
+						</div>
+						<button type="submit" class="btn btn-primary">Connexion</button>
+					</form>
+				</fieldset>
+			</div>
+			<div id="inscription" class="col-lg-7">
+				<div>
+					<fieldset>
+						<legend>Inscription</legend>
+						<form>
+							<div class="form-row">
+								<div class="form-group col-md-6">
+									<label for="inputEmail4">Email</label>
+									<input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+								</div>
+								<div class="form-group col-md-6">
+									<label for="inputPassword4">Mot de Passe</label>
+									<input type="password" class="form-control" id="inputPassword4" placeholder="Mot de Passe">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputAddress">Adresse</label>
+								<input type="text" class="form-control" id="inputAddress" placeholder="1234 Rue Dupont...">
+							</div>
+							<div class="form-group">
+								<label for="inputAddress2">Adresse 2</label>
+								<input type="text" class="form-control" id="inputAddress2" placeholder="Appartement, studio ou étage...">
+							</div>
+							<div class="form-row">
+								<div class="form-group col-md-8">
+									<label for="inputCity">Ville</label>
+									<input type="text" class="form-control" id="inputCity">
+								</div>
+								<div class="form-group col-md-4">
+									<label for="inputZip">Code Postal</label>
+									<input type="text" class="form-control" id="inputZip">
+								</div>
+							</div>
+							<button type="submit" class="btn btn-primary">S'inscrire</button>
+						</form>
+					</fieldset>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
