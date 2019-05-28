ac9734de52919223bcad50058947eb9d5944a4df
Florian APAVOU <florian9303@gmail.com>
2019-05-29 01:29:00 +0200

    star rating

--- php/info_media.php
+++ php/info_media.php
@@ -1,8 +1,8 @@
 <?php
 
-  include "../php/header.php";
-  include "../php/includes.php";
-  include "../php/fonctions.php";
+include "../php/header.php";
+include "../php/includes.php";
+include "../php/fonctions.php";
 
 
 //Debug :
@@ -73,116 +73,138 @@ $commentaires[4] = ["commentaire" => "A la fin, le héros meurt !!!","nom" => "l
 
 ?>
 
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-4">
-        <img src="../imgs/<?=$medias[$id_media]["photo"]?>" class="img-thumbnail imgFM" alt="Responsive image">
-      </div>
-      <div class="col-lg-6">
-        <h1>Infos Pratiques</h1>
-        <br>
-        <table class="table table-hover self-align-center">
-          <tbody>
-            <tr>
-              <th scope="row"><?= $medias[$id_media]["type"]?></th>
-              <td>
-                <div>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-4">
+      <img src="../imgs/<?=$medias[$id_media]["photo"]?>" class="img-thumbnail imgFM" alt="Responsive image">
+    </div>
+    <div class="col-lg-6">
+      <h1>Infos Pratiques</h1>
+      <br>
+      <table class="table table-hover self-align-center">
+        <tbody>
+          <tr>
+            <th scope="row"><?= $medias[$id_media]["type"]?></th>
+            <td>
+              <div>
                 <span id="span_titre"><?= $medias[$id_media]["titre"]?></span>
 
-                </div>
-              </td>
-              <?php
-              if($_SESSION['type_utilisateur'] == "Admin" || $_SESSION['type_utilisateur'] == "Employe")
-              echo "
-              <td><input id=\"input_titre\" value=\"".$medias[$id_media]["titre"]."\" hidden></input>
-              <i id=\"pen_titre\" class=\"fas fa-pen\" onclick=\"bascule_masque('span_titre', 'input_titre', 'pen_titre', 'check_titre')\"></i>
-              <i id=\"check_titre\" class=\"fas fa-check\" onclick=\"bascule_masque('span_titre', 'input_titre', 'pen_titre', 'check_titre'); modifie_titre('".$id_media."')\" hidden></i></td>";
-              ?>
-            </tr>
-            <tr>
-              <th scope="row">Auteur</th>
-              <td>
-                <div>
+              </div>
+            </td>
+            <?php
+            if($_SESSION['type_utilisateur'] == "Admin" || $_SESSION['type_utilisateur'] == "Employe")
+            echo "
+            <td><input id=\"input_titre\" value=\"".$medias[$id_media]["titre"]."\" hidden></input>
+            <i id=\"pen_titre\" class=\"fas fa-pen\" onclick=\"bascule_masque('span_titre', 'input_titre', 'pen_titre', 'check_titre')\"></i>
+            <i id=\"check_titre\" class=\"fas fa-check\" onclick=\"bascule_masque('span_titre', 'input_titre', 'pen_titre', 'check_titre'); modifie_titre('".$id_media."')\" hidden></i></td>";
+            ?>
+          </tr>
+          <tr>
+            <th scope="row">Auteur</th>
+            <td>
+              <div>
                 <span id="span_auteur"><?= $medias[$id_media]["auteur"]?></span>
-                </div>
-              </td>
-              <?php
-              if($_SESSION['type_utilisateur'] == "Admin" || $_SESSION['type_utilisateur'] == "Employe")
-              echo "
-              <td><input id=\"input_auteur\" value=\"".$medias[$id_media]["auteur"]."\" hidden></input>
-              <i id=\"pen_auteur\" class=\"fas fa-pen\" onclick=\"bascule_masque('span_auteur', 'input_auteur', 'pen_auteur', 'check_auteur')\"></i>
-              <i id=\"check_auteur\" class=\"fas fa-check\" onclick=\"bascule_masque('span_auteur', 'input_auteur', 'pen_auteur', 'check_auteur'); modifie_auteur('".$id_media."')\" hidden></i></td>";
-              ?>
-            </tr>
-            <tr>
-              <th scope="row">Prix</th>
-              <td>
-                <div>
+              </div>
+            </td>
+            <?php
+            if($_SESSION['type_utilisateur'] == "Admin" || $_SESSION['type_utilisateur'] == "Employe")
+            echo "
+            <td><input id=\"input_auteur\" value=\"".$medias[$id_media]["auteur"]."\" hidden></input>
+            <i id=\"pen_auteur\" class=\"fas fa-pen\" onclick=\"bascule_masque('span_auteur', 'input_auteur', 'pen_auteur', 'check_auteur')\"></i>
+            <i id=\"check_auteur\" class=\"fas fa-check\" onclick=\"bascule_masque('span_auteur', 'input_auteur', 'pen_auteur', 'check_auteur'); modifie_auteur('".$id_media."')\" hidden></i></td>";
+            ?>
+          </tr>
+          <tr>
+            <th scope="row">Prix</th>
+            <td>
+              <div>
                 <span id="span_prix"><?= $medias[$id_media]["prix"]?> euros</span>
-                </div>
-              </td>
-              <?php
-              if($_SESSION['type_utilisateur'] == "Admin" || $_SESSION['type_utilisateur'] == "Employe")
-              echo "
-              <td><input type=\"number\" id=\"input_prix\" value=\"".$medias[$id_media]["prix"]."\" hidden></input>
-              <i id=\"pen_prix\" class=\"fas fa-pen\" onclick=\"bascule_masque('span_prix', 'input_prix', 'pen_prix', 'check_prix')\"></i>
-              <i id=\"check_prix\" class=\"fas fa-check\" onclick=\"bascule_masque('span_prix', 'input_prix', 'pen_prix', 'check_prix'); modifie_prix('".$id_media."')\" hidden></i></td>";
-              ?>
-            </tr>
-            <tr>
-              <th scope="row">Nombre d'exemplaire restant </th>
-              <td>
-                <div>
+              </div>
+            </td>
+            <?php
+            if($_SESSION['type_utilisateur'] == "Admin" || $_SESSION['type_utilisateur'] == "Employe")
+            echo "
+            <td><input type=\"number\" id=\"input_prix\" value=\"".$medias[$id_media]["prix"]."\" hidden></input>
+            <i id=\"pen_prix\" class=\"fas fa-pen\" onclick=\"bascule_masque('span_prix', 'input_prix', 'pen_prix', 'check_prix')\"></i>
+            <i id=\"check_prix\" class=\"fas fa-check\" onclick=\"bascule_masque('span_prix', 'input_prix', 'pen_prix', 'check_prix'); modifie_prix('".$id_media."')\" hidden></i></td>";
+            ?>
+          </tr>
+          <tr>
+            <th scope="row">Nombre d'exemplaire restant </th>
+            <td>
+              <div>
                 <span id="span_nb_exemplaire"><?= $medias[$id_media]["nb_exemplaire"]?></span>
-                </div>
-              </td>
-              <?php
-              if($_SESSION['type_utilisateur'] == "Admin" || $_SESSION['type_utilisateur'] == "Employe")
-              echo "
-              <td><input type=\"number\" id=\"input_nb_exemplaire\" value=\"".$medias[$id_media]["nb_exemplaire"]."\" hidden></input>
-              <i id=\"pen_nb_exemplaire\" class=\"fas fa-pen\" onclick=\"bascule_masque('span_nb_exemplaire', 'input_nb_exemplaire', 'pen_nb_exemplaire', 'check_nb_exemplaire')\"></i>
-              <i id=\"check_nb_exemplaire\" class=\"fas fa-check\" onclick=\"bascule_masque('span_nb_exemplaire', 'input_nb_exemplaire', 'pen_nb_exemplaire', 'check_nb_exemplaire'); modifie_nb_exemplaire('".$id_media."')\" hidden></i></td>";
-              ?>
-            </tr>
-          </tbody>
-        </table>
-        <?php
-        if($medias[$id_media]["nb_exemplaire"] == 0)
-        echo "
-        <div>
-          <button id=\"notif_bell\" onClick=\"demande_notification('".$id_media."'); bascule_masque('notif_bell')\" class=\"btn btn-info fas fa-bell\" title=\"Me notifier dès sa disponibilité\"></button>
-        </div>";
-        ?>
-      </div>
+              </div>
+            </td>
+            <?php
+            if($_SESSION['type_utilisateur'] == "Admin" || $_SESSION['type_utilisateur'] == "Employe")
+            echo "
+            <td><input type=\"number\" id=\"input_nb_exemplaire\" value=\"".$medias[$id_media]["nb_exemplaire"]."\" hidden></input>
+            <i id=\"pen_nb_exemplaire\" class=\"fas fa-pen\" onclick=\"bascule_masque('span_nb_exemplaire', 'input_nb_exemplaire', 'pen_nb_exemplaire', 'check_nb_exemplaire')\"></i>
+            <i id=\"check_nb_exemplaire\" class=\"fas fa-check\" onclick=\"bascule_masque('span_nb_exemplaire', 'input_nb_exemplaire', 'pen_nb_exemplaire', 'check_nb_exemplaire'); modifie_nb_exemplaire('".$id_media."')\" hidden></i></td>";
+            ?>
+          </tr>
+        </tbody>
+      </table>
+      <?php
+      if($medias[$id_media]["nb_exemplaire"] == 0)
+      echo "
+      <div>
+      <button id=\"notif_bell\" onClick=\"demande_notification('".$id_media."'); bascule_masque('notif_bell')\" class=\"btn btn-info fas fa-bell\" title=\"Me notifier dès sa disponibilité\"></button>
+      </div>";
+      ?>
     </div>
-    <br>
-    <fieldset>
-      <legend>Laisser un commentaire</legend>
-      <form action="#" method="post">
+  </div>
+  <br>
+  <fieldset>
+    <legend>Laisser un commentaire</legend>
+    <form action="#" method="post">
       <div>
         <textarea id="msg" class="form-control" name="message" placeholder="Insérez votre message..."></textarea>
       </div>
-      </br>
-      <div class="d-flex">
-        <input type="submit" class="btn btn-primary ml-auto p-2" value="Envoyer"></input>
+    <div class="d-flex">
+      <div class="star-rating">
+        <input id="star-5" type="radio" name="rating" value="star-5">
+        <label for="star-5" title="5 stars">
+          <i class="active fa fa-star" aria-hidden="true"></i>
+        </label>
+        <input id="star-4" type="radio" name="rating" value="star-4">
+        <label for="star-4" title="4 stars">
+          <i class="active fa fa-star" aria-hidden="true"></i>
+        </label>
+        <input id="star-3" type="radio" name="rating" value="star-3">
+        <label for="star-3" title="3 stars">
+          <i class="active fa fa-star" aria-hidden="true"></i>
+        </label>
+        <input id="star-2" type="radio" name="rating" value="star-2">
+        <label for="star-2" title="2 stars">
+          <i class="active fa fa-star" aria-hidden="true"></i>
+        </label>
+        <input id="star-1" type="radio" name="rating" value="star-1">
+        <label for="star-1" title="1 star">
+          <i class="active fa fa-star" aria-hidden="true"></i>
+        </label>
       </div>
-    </form>
-    <fieldset>
-      <legend>Commentaires</legend>
-      <table class="table table-striped">
-        <?php
-        foreach($commentaires as $commentaire)
-        {
-          echo "<tr>
-              <td>".$commentaire['prenom']." ".$commentaire['nom']."</td>
-              <td>".$commentaire['commentaire']."</td><td>".$commentaire['note']."/5</td>
-            </tr>";
-        }
-        ?>
-      </table>
-    </fieldset>
-  </div>
+      <button type="submit" class="btn btn-primary my-3 ml-auto p-2 py-1" value="Envoyer">Envoyer</button>
+    </div>
+  </form>
+  <br>
+  <fieldset>
+    <legend>Commentaires</legend>
+    <table class="table table-striped">
+      <?php
+      foreach($commentaires as $commentaire)
+      {
+        echo "<tr>
+        <td>".$commentaire['prenom']." ".$commentaire['nom']."</td>
+        <td>".$commentaire['commentaire']."</td><td>".$commentaire['note']."/5</td>
+        </tr>";
+      }
+      ?>
+    </table>
+  </fieldset>
+</div>
 
-  <?php
-    include "../php/footer.php";
-   ?>
+<?php
+include "../php/footer.php";
+?>
