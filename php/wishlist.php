fa0282601f2896a4014b22e6d19d45dd3b004087
Florian APAVOU <florian9303@gmail.com>
2019-05-29 19:15:55 +0200

    exemple de wishlist

--- php/wishlist.php
+++ php/wishlist.php
@@ -15,33 +15,44 @@ include "../php/header.php";
   <div class="container">
 
     <!-- Action (Plus d'infos et Ne plus suivre), mettre class="table-success" sur lignes où les médias disponible atm -->
-    <div class="row">
       <div class="col-lg-12">
         <h1>Vos Notifications</h1>
         <br>
-        <table class="table table-hover table-striped self-align-center">
+        <table class="table table-hover">
           <thead>
             <tr>
-              <th scope="col">
-                Type
-              </th>
               <th scope="col">
                 Titre
               </th>
               <th scope="col">
                 Auteur
               </th>
-              <th scope="col">
+              <th scope="col" class="d-flex justify-content-center">
                 Action
               </th>
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <td>Ch'uis pas rassurer</td>
+              <td>Zambla</td>
+              <td class="d-flex justify-content-center">
+                <button class="btn btn-primary mx-1">Accéder au média</button>
+                <button class="btn btn-danger mx-1">Ne plus suivre</button>
+              </td>
+            </tr>
+            <tr class="table-success">
+              <td>B2O vs 272727 Arrrrrh</td>
+              <td>Baba</td>
+              <td class="d-flex justify-content-center">
+                <button class="btn btn-primary mx-1">Accéder au média</button>
+                <button class="btn btn-danger mx-1">Ne plus suivre</button>
+              </td>
+            </tr>
           </tbody>
         </table>
         <br>
       </div>
-    </div>
 
 
   </div>
