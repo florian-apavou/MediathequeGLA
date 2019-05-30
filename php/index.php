02f21e0d6b67d26772b9e7ae6ce6fa719823d64d
aurélien goulley <aurelien.goulley@gmail.com>
2019-05-30 19:04:13 +0200

    page d'accueil

--- php/index.php
+++ php/index.php
@@ -7,11 +7,45 @@ include "../php/includes.php";
   <h1 class="text-center">Bienvenue à la médiathèque Karine Le Marchand</h1>
   <br>
   <p class="text-center">
-    Présente depuis 2019, l’E-médiathèque KarineLemarchand vous redonnera le sourire. 
-	Ouverte à tous, la médiathèque a pour vocation de mettre à portée de main 
-	toute une offre culturelle sous forme d’ouvrages, de documents audiovisuels et autres. 
-	Avec une interface épuré et facile d’utilisation, 
-	la réservation de documents n’as jamais été aussi simple et rapide. 
+
+  Bienvenue dans l’espace numérique de la médiathèque Karine Le Marchand.
+  Présente depuis 2019, l’E-médiathèque Karine Le Marchand vous redonnera le sourire.
+  </br>
+  </p>
+  <p class="text-center">
+	Avec une interface épurée et facile d’utilisation,
+	la réservation de documents n’as jamais été aussi simple et rapide.
+  Ici, vous retrouverez tous les outils nécessaires pour éveiller votre curiosité, votre détente, votre apprentissage, ou simplement votre divertissement.</br>
+  D’enfant à retraité, en passant par l'adolescent étudiant ou l’adulte actif, une large gamme de produits seront à votre disposition afin de vous satisfaire qui que vous soyiez.
+  </br>
+  </p>
+  <p class="text-center">
+  Passionné de lecture ? Une diversité de livres permettront à tous de trouver la lecture qui vous convienne. Que ce soit pour dévorer un roman de sciences fictions ou étudier une oeuvre connue, la médiathèque Karine Le Marchand vous propose un espace de travail vaste et silencieux afin de vous laisser accomplir tous vos projets littéraires.
+  Aussi attiré par la nature ? N’hésitez pas à bouquiner dans le parc situé juste à côté de la médiathèque !
+  </br>
+  </p>
+  <p class="text-center">
+  Métro, boulot, dodo ! Mais quelle place pour la culture et l’information ? Une multitude de revues scientifiques, historiques, politiques ou artistiques, à lire dans le métro ou le train peuvent redonner la pèche à vos transports en communs en leur donnant cette touche de découverte. Fini la routine !
+  </br>
+  </p>
+  <p class="text-center">
+  Vous avez l’oreille musicale ? Retrouvez vos chanteurs et musiciens favoris dans notre large choix de CD. De quoi savourer vos musiques préférées ou découvrir de nouveaux horizons ! De plus, notre espace multimédia vous permet d’écouter le CD de votre choix si vous doutez de savoir si c’est le bon album ou votre style de musique.
+  </br>
+  </p>
+  <p class="text-center">
+  Cinéphile, vous ne serez pas en reste, et ce, quelque soit votre style de film ! Un documentaire pour vos enfants ou vos élèves ? Ou une soirée Hitchcock entre amis ? Ou simplement un dessin animé pour conserver de bons souvenirs autour d’une boîte de pop-corn ? Tout est possible ! Vous pouvez aussi, seul ou à plusieurs, demander à louer le DVD et le regarder au sein de notre espace multimédia.
+  </br>
+  </p>
+  <p class="text-center">
+  Si louer nos articles un à un ne vous suffit pas, un abonnement mensuel est disponible afin de pouvoir réserver jusqu'à 4 articles simultanéments. De quoi anticiper vos diverses envies culturelles du mois !
+  </br>
+  </p>
+  <p class="text-center">
+  Bonne visite sur notre site,
+  </br>
+  </p>
+  <p class="text-center">
+  L’équipe de la médiathèque Karine Lemarchand.
 	</p>
 
 </br>
