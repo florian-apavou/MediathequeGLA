787da11bf9aa764d94afd8cf91a95d7d7b81f77c
Eokaun <florian9303@gmail.com>
2019-05-30 23:09:33 +0200

    btn Notification

--- php/admin.php
+++ php/admin.php
@@ -12,6 +12,7 @@ include "../php/includes.php";
   <div class="row">
     <a class="btn btn-primary gestClientBox col-lg-5 mx-1 my-1 d-flex justify-content-center align-items-center <?php if(isset($_SESSION['rang']) && $_SESSION['rang'] == "gestMedia"){echo " disabled ";} ?>" href="gestClients.php" ><i class="fas fa-address-book fa-5x mx-2"></i><span>Gestion des comptes Clients</span></a>
     <a class="btn btn-secondary gestMediaBox col-lg-5 mx-1 my-1 d-flex justify-content-center align-items-center  <?php if(isset($_SESSION['rang']) && $_SESSION['rang'] == "gestClient"){echo " disabled ";} ?>" href="gestMedias.php"><i class="fas fa-book fa-5x mx-2"></i><span>Ajout de Médias</span></a>
+      <a class="btn btn-warning gestMediaBox col-lg-10 mx-1 my-1 d-flex justify-content-center align-items-center" href=""><i class="fas fa-book fa-5x mx-2"></i><span>Gestion Notifications</span></a>
   </div>
   <!-- ajouter disabled en fonction du rang -->
 
